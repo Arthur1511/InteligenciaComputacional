63f6e59fb10f32ae61e8a6111c1d6d56088ba7ba
Arthur Bernardo <arthur.bernardo1511@gmail.com>
2018-09-16 15:48:38 -0300

    Print para exibir tempo de execução descomentado

--- minimalPath.py
+++ minimalPath.py
@@ -5,12 +5,12 @@ import timeit
 
 inicio = timeit.default_timer()
 
-# distancias, qtdCidades = funcoes.calcularDistancias("A280.txt")
-distancias, qtdCidades = funcoes.lerDados("cv10.txt")
+distancias, qtdCidades = funcoes.calcularDistancias("A280.txt")
+# distancias, qtdCidades = funcoes.lerDados("cv10.txt")
 
 # for i in range(qtdCidades):
 
-rota, fo = funcoes.vizinhoMaisProximo(distancias, qtdCidades, 4, 0.5)
+rota, fo = funcoes.vizinhoMaisProximo(distancias, qtdCidades, 0, 0.5)
 
 # rota, fo = funcoes.insercaoMaisBarata(distancias, qtdCidades)
 
@@ -18,6 +18,8 @@ rota_refinada, fo_refinada = funcoes.descida(distancias, rota, qtdCidades, fo)
 
 print("Rota ->", rota, "\n", "FO=", fo)
 print("Rota Refinada ->", rota_refinada, "\n", "FO Refinado=", fo_refinada)
+
 fim = timeit.default_timer()
 
-# print("Tempo de execução(s):", fim-inicio)
+print("Tempo de execução(s):", fim-inicio)
+
